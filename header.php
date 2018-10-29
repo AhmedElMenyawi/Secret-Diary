e503820cc8228f0c3ec5ed1f1129df21ae14e1c6
Ahmed ElMenyawi <Menyawi73@gmail.com>
2018-10-30 01:48:33 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <style type="text/css">
+        html{
+            background: url(background.jpg) no-repeat center center fixed;
+            -webkit-background-size : cover;
+            -moz-background-size : cover;
+            -o-background-size : cover;
+            background-size : cover;
+        }
+        body{
+            background: none;
+        }
+        .container{
+            text-align: center;
+            width: 400px;
+            
+        }
+        #homepage{
+            margin-top: 120px;
+        }
+        #login-form{
+            display: none;
+        }
+        .toggleclass{
+            font-weight: bold;
+        }
+        #diary{
+            width:100%;
+            height: 650px;
+            margin-top: 20px;
+        }
+    </style>
+    <title>SecretDiary</title>
+  </head>
+  <body>
